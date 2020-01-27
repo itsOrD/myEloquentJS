024695fc818790248964643d3455caea8b1d9bfb
Matthew Beckerleg <ord.sc2@gmail.com>
2020-01-27 00:53:43 -0800

    recursion prompt answered

--- Chapter3/Recursion/recursion.js
+++ Chapter3/Recursion/recursion.js
@@ -8,9 +8,11 @@ use these rules:
 	* for any other number N, its evenness is the same as N - 2
 */
 
+// my solution:
 const isEven = (N) => {
   if (N === 0) return true;
   if (N === 1) return false;
+  if (N < 0) return isEven(-N);
   else return isEven(N - 2);
 }
 
@@ -19,3 +21,13 @@ const isEven = (N) => {
 console.log(isEven(50));
 console.log(isEven(75));
 console.log(isEven(-1)); // <- call stack error before
+
+/*
+eloquentjs solution:
+function isEven(n) {
+  if (n == 0) return true;
+  else if (n == 1) return false;
+  else if (n < 0) return isEven(-n);
+  else return isEven(n - 2);
+}
+*/
