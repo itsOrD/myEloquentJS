07fd6f059c257f45b326454e1bdd27d15c603747
Matthew Beckerleg <ord.sc2@gmail.com>
2020-02-01 21:37:07 -0800

    more ch5 notes

---
+++ Chapter5/Flattening/myFlatten.js
@@ -0,0 +1,9 @@
+// use the reduce method in combination with the concat method to "flatten" an array of arrays into a single array that has
+// all the elements of the original arrays.
+
+// my solution:
+const myFlatten = (arr) => {
+	
+};
+
+// console.log(myFlatten());
