81cbb14d294025ce55c8cb7f8b84f599aefa4d7a
Matthew Beckerleg <ord.sc2@gmail.com>
2020-02-02 20:11:42 -0800

    flatten function using reduce

--- Chapter5/Flattening/myFlatten.js
+++ Chapter5/Flattening/myFlatten.js
@@ -1,9 +1,16 @@
-// use the reduce method in combination with the concat method to "flatten" an array of arrays into a single array that has
-// all the elements of the original arrays.
+// use the reduce method in combination with the concat method to "flatten" an array of arrays into 
+// a single array that has all the elements of the original arrays.
 
 // my solution:
 const myFlatten = (arr) => {
-	
+	return arr.reduce((answerArr, current) => {
+    return answerArr.concat(current);
+  }, [])
 };
 
-// console.log(myFlatten());
+console.log(myFlatten([[1, 2, 3], [4, 5], [6]]));
+
+// solution from https://eloquentjavascript.net/code/#5.1
+let arrays = [[1, 2, 3], [4, 5], [6]];
+
+console.log(arrays.reduce((flat, current) => flat.concat(current), []));
